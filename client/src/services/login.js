238fbe63374fc5083ecc06120dc655f666e8b00c
Neringagaig <ngaigalaite@yahoo.com>
2022-11-13 01:02:36 +0200

    fixed login error

--- client/src/services/login.js
+++ client/src/services/login.js
@@ -2,8 +2,8 @@ import axios from "../api/axios";
 
 const LOGIN_URL = `/login`;
 
-const login = async () => {
-  const response = await axios.post(LOGIN_URL);
+const login = async (credentials) => {
+  const response = await axios.post(LOGIN_URL, credentials);
   return response.data;
 };
 
